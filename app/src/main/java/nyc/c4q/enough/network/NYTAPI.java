ef18bc656507e55194e7fc349668cb1a2558378d
Amar Ahmed <amar11432@gmail.com>
2018-03-03 17:34:13 -0500

    added endpoints

---
+++ app/src/main/java/nyc/c4q/enough/network/NYTAPI.java
@@ -0,0 +1,32 @@
+package nyc.c4q.enough.network;
+
+/**
+ * Created by C4Q on 3/3/18.
+ */
+import nyc.c4q.enough.model.Results;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface NYTAPI {
+
+    String NYT_Top_APIKey = "d752126a9dec4034b7f3e0e71ab84825";
+
+
+    @GET("svc/topstories/v2/books.json")
+    Call<Results> getNYTbooks(@Path ("section") String section, @Query("apikey") String apikey);
+
+    @GET("svc/topstories/v2/travel.json")
+    Call<Results> getNYTtravel(@Path ("section") String section, @Query("apikey") String apikey);
+
+    @GET("svc/topstories/v2/fashion.json")
+    Call<Results> getNYTfashion(@Path ("section") String section, @Query("apikey") String apikey);
+
+
+
+
+
+}
