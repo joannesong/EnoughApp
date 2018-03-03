6b858cc098136ad3d003c56dbd130c9a1c96ef6e
matin2510 <mustafizurmatin@ac.c4q.nyc>
2018-03-03 12:15:48 -0500

    mmHelpActivty

---
+++ app/src/main/java/nyc/c4q/enough/controller/FragmentAdapter.java
@@ -0,0 +1,41 @@
+package nyc.c4q.enough.controller;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import nyc.c4q.enough.fragments.HelpFragment;
+
+/**
+ * Created by c4q on 3/3/18.
+ */
+
+public class FragmentAdapter extends FragmentStatePagerAdapter {
+    private int numberOfTabs;
+
+
+    public FragmentAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numberOfTabs = numOfTabs;
+    }
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return new HelpFragment();
+            /*case 1:
+                return new TimeswireFragment();
+            case 2:
+                return new MovieReviewsFragment();*/
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+         return numberOfTabs;
+    }
+}
