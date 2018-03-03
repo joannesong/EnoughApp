d4e393b90d9a53352f499789e465d8ba322e11e5
Joanne Song <joannesong@ac.c4q.nyc>
2018-03-03 12:38:29 -0500

    added a recyclerview on the data fragment

---
+++ app/src/main/java/nyc/c4q/enough/fragments/DataFragment.java
@@ -0,0 +1,38 @@
+package nyc.c4q.enough.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nyc.c4q.enough.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DataFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_data, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        return view;
+
+    }
+
+}
