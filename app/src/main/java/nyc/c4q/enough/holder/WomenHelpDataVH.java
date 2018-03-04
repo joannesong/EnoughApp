b58ee6368e7babc8a4a235681e34679bd64f42c1
bRad-95 <bryantramirez@ac.c4q.nyc>
2018-03-03 21:30:40 -0500

    resourcesFragmentCallIntent

--- app/src/main/java/nyc/c4q/enough/holder/WomenHelpDataVH.java
+++ app/src/main/java/nyc/c4q/enough/holder/WomenHelpDataVH.java
@@ -1,6 +1,8 @@
 package nyc.c4q.enough.holder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -17,17 +19,19 @@ public class WomenHelpDataVH extends RecyclerView.ViewHolder {
     private TextView organizationName;
     private TextView organizationLink;
     private LinearLayout callLayout;
+    private Context context;
+
 
     public WomenHelpDataVH(View itemView) {
         super(itemView);
-
+        context = itemView.getContext();
         organizationName = itemView.findViewById(R.id.organization_name);
         organizationLink = itemView.findViewById(R.id.organization_site);
         callLayout = itemView.findViewById(R.id.press_call_layout);
-
     }
 
-    public void onBind(final WomenDataResults womenDataResults){
+
+    public void onBind(final WomenDataResults womenDataResults) {
 
         organizationName.setText(womenDataResults.getOrganizationname());
         organizationLink.setText(womenDataResults.getUrl());
@@ -36,10 +40,12 @@ public class WomenHelpDataVH extends RecyclerView.ViewHolder {
             @Override
             public void onClick(View v) {
                 //put womenDataResults.getPhone in here
-
+                String phone = womenDataResults.getPhone();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phone));
+                context.startActivity(intent);
             }
         });
-
 
     }
 }
