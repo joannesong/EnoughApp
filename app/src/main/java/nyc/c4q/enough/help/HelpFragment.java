54860075d38fe113c43ccbc4b86a386c34206021
Joanne Song <joannesong@ac.c4q.nyc>
2018-03-04 14:15:10 -0500

    Added Shared Preferences in News Activity/ Help Fragment. This will utilize the distress text messages

--- app/src/main/java/nyc/c4q/enough/help/HelpFragment.java
+++ app/src/main/java/nyc/c4q/enough/help/HelpFragment.java
@@ -1,6 +1,8 @@
 package nyc.c4q.enough.help;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -9,7 +11,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import nyc.c4q.enough.NewsActivity;
 import nyc.c4q.enough.R;
 
 /**
@@ -19,6 +23,13 @@ public class HelpFragment extends Fragment {
     private final static String TAG = "RESULTS";
     private RecyclerView contactsRecycler;
     private LinearLayoutManager linearLayoutManager;
+
+    private EditText distressMessage;
+    private String message;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private static final String SHARED_PREFS_KEY = "womenSP";
 
 
     public HelpFragment() {
@@ -35,6 +46,14 @@ public class HelpFragment extends Fragment {
         linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         contactsRecycler.setHasFixedSize(true);
         contactsRecycler.setLayoutManager(linearLayoutManager);
+
+        distressMessage = rootView.findViewById(R.id.distress_edit);
+        message = distressMessage.getText().toString();
+
+        sharedPreferences = getContext().getSharedPreferences(SHARED_PREFS_KEY, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.putString("message", message);
+
 
         return rootView;
     }
